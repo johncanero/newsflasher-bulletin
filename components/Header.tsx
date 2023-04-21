8ebe28fe0f3d436ad9248bb4c4216c3084fa57d3
John Canero <johncanero.official@gmail.com>
2023-04-21 08:46:01 +0800

    Adjust Heading Content

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,8 +11,8 @@ function Header() {
 
                 {/* Link */}
                 <Link href={'/'} prefetch={false}>
-                    <h1 className="text-lg md:text-3xl flex-1 text-center">
-                        <span className="decoration-6 decoration-orange-400">
+                    <h1 className="text-lg md:text-3xl flex-1 text-center font-medium">
+                        <span className="">
                             NewsFlasher
                         </span>{' '}
                         Bulletin
@@ -22,7 +22,7 @@ function Header() {
                 {/* Dark Mode */}
                 <div className="flex items-center justify-end space-x-2">
                     {/* Dark mode button */}
-                    
+
                     {/* Button */}
                     <button className="hidden md:inline bg-lime-500 text-stone-800 font-semibold px-4 lg:px-8 py-2 lg:py-3 rounded-full dark:bg-lime-400">
                         Subscribe Now
