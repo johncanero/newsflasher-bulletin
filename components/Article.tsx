fcca616e3eb4d4d3ed0721f9973251a757df298e
John Canero <johncanero.official@gmail.com>
2023-04-22 16:29:48 +0800

    Add Read More Button

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,13 +1,13 @@
-// import LiveTimestamp from './LiveTimestamp';
-// import ReadMore from './ReadMore';
 import Image from 'next/image';
+import ReadMoreButton from './ReadMoreButton';
+// import LiveTimestamp from './LiveTimestamp';
 
 type Props = {
 	article: Article;
 };
 function Article({ article }: Props) {
 	return (
-		<article className="flex flex-col transition-all duration-200 ease-out rounded-lg shadow-md bg-slate-100 dark:bg-slate-800 hover:scale-105 hover:shadow-lg hover:bg-slate-200">
+		<article className="flex flex-col transition-all duration-200 ease-out rounded-lg shadow-md bg-slate-100 dark:bg-zinc-800 hover:scale-105 hover:shadow-lg hover:bg-slate-200">
 			{article.image && (
 				// eslint-disable-next-line @next/next/no-img-element
 				<img
@@ -24,17 +24,15 @@ function Article({ article }: Props) {
 					<section className="flex-1 mt-2">
 						<p className="text-xs line-clamp-6">{article.description}</p>
 					</section>
-					<footer className="flex pt-5 ml-auto space-x-1 text-xs italic text-right text-gray-400">
+					<footer className="flex pt-5 ml-auto space-x-1 text-xs italic text-right text-gray-300">
 						<p>{article.source} -</p>
 						<p>
 							{/* <LiveTimestamp time={article.published_at} /> */}
 						</p>
 					</footer>
 				</div>
-				{/* READ more button */}
-				{/* <ReadMore article={article} /> */}
-
-
+				{/* READ More button */}
+				<ReadMoreButton article={article} />
 			</div>
 		</article>
 	);
